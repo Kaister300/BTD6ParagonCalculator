36b45d3cd248683583fb8c7028a99641984027c8
Kaister300 <109926616+Kaister300@users.noreply.github.com>
2023-06-25 02:58:27 +1000

    Added Damage Calculator
    - Takes Degree from Calculator
    - Loads JSON file of relevant tower statistics
    - Applies relevant damage calculations to statistics
    - Displays results to user

--- project/webpage/paragondamage.js
+++ project/webpage/paragondamage.js
@@ -27,20 +27,143 @@ class paragonDamage extends LitElement {
 
     constructor() {
         super();
-        this.degree = 0;
-        window.addEventListener("degree", (e) => this.degree = e.detail.currDegree);
+        window.addEventListener("degree", (e) => this.degree = parseInt(e.detail.currDegree));
+        this.degree = 1;
+        this.setDefault();
+    }
+
+    setDefault() {
+        this._paragon = void 1;
+        this.name = "--------"
+        this.damage = "N/A";
+        this.ceramic = "N/A";
+        this.moab = "N/A";
+        this.boss = "N/A";
+        this.elite = "N/A";
+        this.pierce = "N/A";
+        this.speed = "N/A";
+        this.cooldown = "N/A";
+    }
+
+    _grabSelected(e) {
+        if(e.target.value === "") {
+            this.setDefault();
+        }
+        else {
+            fetch(`${window.location.href}paragondetails/${e.target.value}.json`)
+            .then(response => response.json())
+            .then(data => this._paragon = data.paragon)
+            .then(data => console.log(this._paragon));
+        }
+    }
+
+    _damageComputation() {
+        this.name = this._paragon.name;
+        let gamma = 1+0.01*(this.degree-1);
+        this._baseDamage(gamma);
+        this.ceramic = this.damage + this._specialDamage(this._paragon.ceramic, gamma);
+        this.moab = this.damage + this._specialDamage(this._paragon.moab, gamma);
+        this._bossDamage(gamma);
+        this._eliteDamage(gamma);
+        this._pierceDamage(gamma);
+        this._attackSpeed();
+    }
+
+    _baseDamage(gamma) {
+        if(this.degree === 1) {
+            this.damage = this._paragon.damage;
+        }
+        else if(this.degree === 100) {
+            this.damage = 2*this._paragon.damage + 10;
+        }
+        else {
+            this.damage = this._paragon.damage*gamma + (this.degree/10) - 1;
+        }
+    }
+
+    /*
+        This includes Ceramics, MOABs and Fortified bonus damage.
+        returns: bonus damage that is applied ontop of base damage
+    */
+    _specialDamage(special, gamma) {
+        if(this.degree === 1) {
+            return special;
+        }
+        else if(this.degree === 100) {
+            return 2*special;
+        }
+        else {
+            return special*gamma;
+        }
+    }
+
+    // Calculates Boss Bloon Damage. Returns nothing
+    _bossDamage(gamma) {
+        if(this.degree === 1) {
+            this.boss = this.damage + this.moab + this._paragon.boss;
+        }
+        else {
+            let extra = this._paragon.boss*gamma;
+            //let x = this.damage + this.moab + 2*extra;
+            //let y = 1+0.2*(this.degree/20);
+            //this.boss = x*y - extra;
+            this.boss = (this.damage + this.moab + 2*extra)*(1+0.2*(this.degree/20)) - extra;
+        }
+    }
+
+    _eliteDamage() {
+        this.elite = 2*this.boss;
+    }
+
+    _pierceDamage(gamma) {
+        if(this.degree === 1) {
+            this.pierce = this._paragon.pierce;
+        }
+        else if(this.degree === 100) {
+            this.pierce = 2*this._paragon.pierce + 100
+        }
+        else {
+            this.pierce = this._paragon.pierce*gamma + (this.degree-1);
+        }
+    }
+
+    _attackSpeed() {
+        if(this.degree === 1) {
+            this.speed = this._paragon.speed;
+        }
+        else {
+            this.speed = this._paragon.speed / (1+0.01*Math.sqrt(50*this.degree - 50));
+        }
     }
 
     render() {
+        if(this._paragon) {
+            this._damageComputation();
+        }
         return html`
         <div>
             <label for="paragon">Choose Paragon: </label>
-            <select>
-                <option value="nothing">Please choose an option</option>
-                <option value="dart">Dart Monkey</option>
-                <option value="boomerang">Boomerang Monkey</option>
-                <option value="ninja">Ninja Monkey</option>
+            <select id="paragon" @change=${this._grabSelected}>
+                <option value="">Please choose an option</option>
+                <optgroup label="Primary">
+                    <option value="dart">Dart Monkey</option>
+                    <option value="boomerang">Boomerang Monkey</option>
+                </optgroup>
+                <optgroup label="Magic">
+                    <option value="ninja">Ninja Monkey</option>
+                </optgroup>
             </select>
+        </div>
+        <div>
+            <h2>${this.name}</h2>
+            <h3>Degree ${this.degree}</h3>
+            <p>Base Damage: ${this.damage}</p>
+            <p>Ceramic Damage: ${this.ceramic}</p>
+            <p>MOAB Damage: ${this.moab}</p>
+            <p>Boss Bloon Damage: ${this.boss}</p>
+            <p>Elite Boss Bloon Damage: ${this.elite}</p>
+            <p>Pierce: ${this.pierce}</p>
+            <p>Attack Speed: ${this.speed}</p>
         </div>
         `;
     }
